95dea134606a7ba3f55d75fabd553d13718dde35
Michael Fleming <Michael@The-Original-Michaels-MacBook-Pro.local>
2013-10-10 16:08:40 -0400

    Form validation cont. ,User exists, Removed unnecessary fields

--- Controller modules/UserSignup/js/scripts.js
+++ Controller modules/UserSignup/js/scripts.js
@@ -1,27 +1,41 @@
 var chatApp = angular.module("chatApp", ["firebase"]);
 
-function loginCtrl($scope, angularFire) {
+function loginCtrl($scope, angularFireCollection) {
 	//links up to firebase and grabs all the users and stores it in the scope.users array
-	var url = new Firebase("https://gamerscafe.firebaseio.com/gamerscafe/users");
-	$scope.users = [];
-	angularFire(url, $scope, "users");
+	var usersUrl = new Firebase("https://gamerscafe.firebaseio.com/gamerscafe/users");
+	$scope.users = angularFireCollection(usersUrl);
 	
 	
 	var userRef = new Firebase("https://gamerscafe.firebaseio.com/");
 	//uses facebook login to grab the user info and prepopulate all the fields
 	var authenticate = new FirebaseSimpleLogin(userRef, function(error, user) {
 		if (user) {
+			console.log(user);
 			//generates a url to get the image based on their unique username
-			$scope.picurl = "http://graph.facebook.com/" + user.username + "/picture?type=large";
-			$scope.fullname = user.displayName;
-			$scope.username = user.username;
-			$scope.email = user.email;
+			var picurl = "http://graph.facebook.com/" + user.username + "/picture?type=large";
+			
+			var userExists = false;
+			for (var i = 0, max = $scope.users.length; i<max; i++) {
+				if ($scope.users[i].email != user.email) {
+					userExists = false;
+				} else {
+					userExists = true;
+					break;
+				}
+			}
+			if (userExists) {
+				console.log("user email exists");
+			} else {
+				console.log("user email does not exist");
+				//creates a user object from all of the fields and pushes it to the firebase table
+				$scope.users.add({"displayName": user.name, "email": user.email, "profilePic": picurl, "facebook": true});
+			}
+			
 		} else {
 			//visual feedback of error
 			console.log("No user Detected");
 		}
 	});
-	
 	
 	$scope.login = function(){
 		//calls the login function of the firebase login
@@ -32,8 +46,26 @@ function loginCtrl($scope, angularFire) {
 	};
 		
 	$scope.addUser = function(){
-		//creates a user object from all of the fields and pushes it to the firebase table
-		$scope.users.push({"displayName": $scope.fullname, "username": $scope.username, "email": $scope.email,"bio": $scope.userbio, "password": $scope.password, "profilePic": $scope.picurl});
+		var userExists = false;
+		for (var i = 0, max = $scope.users.length; i<max; i++) {
+			if ($scope.users[i].email != $scope.user.email) {
+				userExists = false;
+			} else {
+				userExists = true;
+				break;
+			}
+		}
+		if (userExists) {
+			console.log("userExists");
+		} else {
+			console.log("user does not exist");
+			//creates a user object from all of the fields and pushes it to the firebase table
+			$scope.users.add({"displayName": $scope.user.name,"facebook": false, "email": $scope.user.email, "password": $scope.user.password});
+		}	
+	
+		$scope.fullname = "";
+		$scope.password = "";
+		$scope.email = ""; 
 	};
 
 }
