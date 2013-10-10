40415c65d5c0d02b612c0a3c062910d35b71e816
Michael Fleming <Michael@The-Original-Michaels-MacBook-Pro.local>
2013-10-10 10:33:09 -0400

    Css form validation, code cleanup
    removed unneeded functions and added css form validation using ng-valid
    directives

--- Controller modules/UserSignup/js/scripts.js
+++ Controller modules/UserSignup/js/scripts.js
@@ -5,30 +5,27 @@ function loginCtrl($scope, angularFire) {
 	$scope.users = [];
 	angularFire(url, $scope, "users");
 	
-	$scope.login = function(){
-		authenticate.login('facebook', {
-			scope: "email"
-		});
-	};
-	
 	var userRef = new Firebase("https://gamerscafe.firebaseio.com/");
 	var authenticate = new FirebaseSimpleLogin(userRef, function(error, user) {
 		console.log(user);
 		fbUser = user;
 		if (user) {
-			displayUserInfo();
+			$scope.picurl = "http://graph.facebook.com/" + fbUser.username + "/picture";
+			$scope.fullname = fbUser.displayName;
+			$scope.username = fbUser.username;
+			$scope.email = fbUser.email;
 		} else {
-			alert("Incorrect login, Try using an email instead");
+			console.log("No user Detected");
 		}
 	});
 	
-	var displayUserInfo = function(){
-		$scope.picurl = "http://graph.facebook.com/" + fbUser.username + "/picture";
-		$scope.fullname = fbUser.displayName;
-		$scope.username = fbUser.username;
-		$scope.email = fbUser.email;
-	};
 	
+	$scope.login = function(){
+		authenticate.login('facebook', {
+			scope: "email"
+		});
+	};
+		
 	$scope.addUser = function(){
 		$scope.users.push({"displayName": $scope.fullname, "username": $scope.username, "email": $scope.email,"bio": $scope.userbio, "password": $scope.password, "profilePic": $scope.picurl});
 	};
