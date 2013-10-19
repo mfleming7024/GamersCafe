30c405e8acda78f339ba1c0a5faad04d004b3f74
eangeles <niwre2007@gmail.com>
2013-10-19 11:31:21 -0400

    updated staff/games view and intergrated angular

--- gamerscafe_final/views/js/main.js
+++ gamerscafe_final/views/js/main.js
@@ -1,110 +1,114 @@
 var gamerscafe = angular.module('gamerscafe', ['firebase']);
 
 gamerscafe.run(['angularFireAuth', '$rootScope', function(angularFireAuth, $rootScope){
-	var url = new Firebase("https://gamerscafe.firebaseio.com/");
-	angularFireAuth.initialize(url, {name:'user', scope: $rootScope,callback: function(err, user) {
-      
+    var url = new Firebase("https://gamerscafe.firebaseio.com/");
+    angularFireAuth.initialize(url, {name:'user', scope: $rootScope,callback: function(err, user) {
+
     }});
 }]);
 
 gamerscafe.config(function ($routeProvider){
-	$routeProvider
-	.when("/",{
-		controller:"Core",
-		templateUrl:"views/templates/home.html"
-	})
+    $routeProvider
+        .when("/",{
+            controller:"Core",
+            templateUrl:"views/templates/home.html"
+        })
 
-	.when("/login", {
-		controller:"Core",
-		templateUrl:"views/templates/login.html",
-	})
+        .when("/login", {
+            controller:"Core",
+            templateUrl:"views/templates/login.html"
+        })
 
-	.when("/staff", {
-		controller:"Core",
-		templateUrl:"views/templates/staff.html",
-	})
+        .when("/staff", {
+            controller:"Core",
+            templateUrl:"views/templates/staff.html"
+        })
 
-	.when("/admin",{
-		controller:"Core",
-		templateUrl:"views/templates/admin.html"
-	})
+        .when("/admin",{
+            controller:"Core",
+            templateUrl:"views/templates/admin.html"
+        })
 
-	.when("/staff", {
-		controller:"Core",
-		templateUrl:"views/templates/staff.html",
-	})
+        .when("/staff", {
+            controller:"Core",
+            templateUrl:"views/templates/staff.html"
+        })
 
-	.when("/admin_users", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_users.html",
-	})
+        .when("/admin_users", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_users.html"
+        })
 
-	.when("/admin_games", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_games.html",
-	})
+        .when("/admin_games", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_games.html"
+        })
 
-	.when("/admin_station", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_station.html",
-	})
+        .when("/admin_station", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_station.html"
+        })
 
-	.when("/admin_staff", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_staff.html",
-	})
+        .when("/admin_staff", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_staff.html"
+        })
 
-	.when("/admin_add_game", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_add_game.html",
-	})
+        .when("/admin_add_game", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_add_game.html"
+        })
 
-	.when("/admin_add_station", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_add_station.html",
-	})
+        .when("/admin_add_station", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_add_station.html"
+        })
 
-	.when("/admin_add_system", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_add_system.html",
-	})
+        .when("/admin_add_system", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_add_system.html"
+        })
 
-	.when("/admin_systems", {
-		controller:"Core",
-		templateUrl:"views/templates/admin_systems.html",
-	})
+        .when("/admin_systems", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_systems.html"
+        })
 
-	.when("/gts_add_gamer", {
-		controller:"Core",
-		templateUrl:"views/templates/gts_add_gamer.html",
-	})
+        .when("/gts_add_gamer", {
+            controller:"Core",
+            templateUrl:"views/templates/gts_add_gamer.html"
+        })
 
-	.when("/gts_add_q", {
-		controller:"Core",
-		templateUrl:"views/templates/gts_add_q.html",
-	})
+        .when("/gts_add_q", {
+            controller:"Core",
+            templateUrl:"views/templates/gts_add_q.html"
+        })
 
-	.when("/gts_cancel", {
-		controller:"Core",
-		templateUrl:"views/templates/gts_cancel.html",
-	})
+        .when("/gts_cancel", {
+            controller:"Core",
+            templateUrl:"views/templates/gts_cancel.html"
+        })
+        .when("/admin_add_staff", {
+            controller:"Core",
+            templateUrl:"views/templates/admin_add_staff.html"
+        })
 
-	.when("/gts_edit_gamer/:staionId", {
-		controller:"Core",
-		templateUrl:"views/templates/gts_edit_gamer.html",
-	})
+        .when("/gts_edit_gamer/:staionId", {
+            controller:"Core",
+            templateUrl:"views/templates/gts_edit_gamer.html"
+        })
 
-	.otherwise({redirectTo:"/"});
+        .otherwise({redirectTo:"/"});
 
 }).directive('autoComplete', function($timeout) {
-    return function(scope, iElement, iAttrs) {
+        return function(scope, iElement, iAttrs) {
             iElement.autocomplete({
                 source: scope[iAttrs.uiItems],
                 select: function() {
                     $timeout(function() {
-                      iElement.trigger('input');
+                        iElement.trigger('input');
                     }, 0);
                 }
             });
-    };
-});
+        };
+    });
