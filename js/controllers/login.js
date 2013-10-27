c6d5ae9974763d85fce83d27863dcc4d85756e52
eangeles <niwre2007@gmail.com>
2013-10-26 20:59:47 -0400

    [delivers #59620332]site is updated to an MVC structure

---
+++ js/controllers/login.js
@@ -0,0 +1,61 @@
+gamerscafe.controller('Login', ['$scope', '$routeParams', '$location', 'angularFireCollection', 'angularFireAuth', function mtCtrl($scope, $routeParams, $location, angularFireCollection, angularFireAuth){
+
+    var theUser;
+    //****** Facebook Login
+    //Checks if the user is login
+    $scope.$on("angularFireAuth:login", function(evt, user) {
+        if (user.provider == "facebook") {
+            console.log("logged in", user);
+            $scope.picurl = "http://graph.facebook.com/" + user.username + "/picture?type=small";
+            $scope.displayName = user.displayName;
+
+            theUser = user;
+        } else {
+
+        }
+    })
+
+    $scope.login = function() {
+        // when the user login successfully then run the following function
+        angularFireAuth.login('facebook').then(function() {
+            // If the user login successfully it will take them to admin page
+
+            user = theUser;
+
+            console.log(user);
+            $location.path('/admin');
+            if (user) {
+                console.log('test2');
+                //checks the database against what user email is passed in to see if it
+                //exists then sets a boolean to say so
+                var userExists = false;
+                for (var i = 0, max = $scope.users.length; i<max; i++) {
+                    if ($scope.users[i].email != user.email) {
+                        userExists = false;
+                    } else {
+                        userExists = true;
+                        break;
+                    }
+                }
+                if (userExists) {
+                    console.log("user email exists");
+                    //login the user
+                } else {
+                    //FB profile image
+                    var picurl = "http://graph.facebook.com/" + user.username + "/picture?type=small";
+                    //creates a user object from all of the fields and pushes it to the firebase table
+                    $scope.users.add({"displayName": user.name, "email": user.email, "profilePic": picurl, "facebook": true});
+                }
+            } else {
+                //visual feedback of error
+                console.log("No user Detected");
+            }
+        });
+    }
+    //Logs out of Facebook
+    $scope.logout = function() {
+        angularFireAuth.logout();
+        $location.path('/home')
+    };
+
+}])
--- js/main.js
+++ js/main.js
@@ -10,128 +10,128 @@ gamerscafe.config(function ($routeProvider){
 
         .when("/",{
             controller:"Core",
-            templateUrl:"views/templates/home.html",
+            templateUrl:"views/home.html",
             authRequired: false
         })
 
         .when("/login", {
             controller:"Core",
-            templateUrl:"views/templates/login.html",
+            templateUrl:"views/login.html",
             authRequired: false
         })
 
         .when("/staff", {
             controller:"Core",
-            templateUrl:"views/templates/staff.html",
+            templateUrl:"views/staff.html",
             authRequired: true
         })
 
         .when("/admin",{
             controller:"Core",
-            templateUrl:"views/templates/admin.html",
+            templateUrl:"views/admin.html",
             authRequired: true
         })
 
         .when("/admin_users", {
             controller:"Core",
-            templateUrl:"views/templates/admin_users.html",
+            templateUrl:"views/admin_users.html",
             authRequired: true
         })
 
         .when("/admin_games", {
             controller:"Core",
-            templateUrl:"views/templates/admin_games.html",
+            templateUrl:"views/admin_games.html",
             authRequired: true
         })
 
         .when("/admin_station", {
             controller:"Core",
-            templateUrl:"views/templates/admin_station.html",
+            templateUrl:"views/admin_station.html",
             authRequired: true
         })
 
         .when("/admin_staff", {
             controller:"Core",
-            templateUrl:"views/templates/admin_staff.html",
+            templateUrl:"views/admin_staff.html",
             authRequired: true
 
         })
 
         .when("/admin_add_game", {
             controller:"Core",
-            templateUrl:"views/templates/admin_add_game.html",
+            templateUrl:"views/admin_add_game.html",
             authRequired: true
         })
 
         .when("/admin_add_station", {
             controller:"Core",
-            templateUrl:"views/templates/admin_add_station.html",
+            templateUrl:"views/admin_add_station.html",
             authRequired: true
         })
         .when("/admin_add_staff", {
             controller:"Core",
-            templateUrl:"views/templates/admin_add_staff.html",
+            templateUrl:"views/admin_add_staff.html",
             authRequired: true
         })
 
         .when("/admin_add_system", {
             controller:"Core",
-            templateUrl:"views/templates/admin_add_system.html",
+            templateUrl:"views/admin_add_system.html",
             authRequired: true
         })
 
         .when("/admin_systems", {
             controller:"Core",
-            templateUrl:"views/templates/admin_systems.html",
+            templateUrl:"views/admin_systems.html",
             authRequired: true
         })
 
         .when("/gts_add_gamer/:user/:stationId", {
             controller:"Core",
-            templateUrl:"views/templates/gts_add_gamer.html",
+            templateUrl:"views/gts_add_gamer.html",
             authRequired: true
         })
 
         .when("/gts_add_q", {
             controller:"Core",
-            templateUrl:"views/templates/gts_add_q.html",
+            templateUrl:"views/gts_add_q.html",
             authRequired: true
         })
 
         .when("/gts_cancel/:stationId", {
             controller:"Core",
-            templateUrl:"views/templates/gts_cancel.html",
+            templateUrl:"views/gts_cancel.html",
             authRequired: true
         })
 
         .when("/gts_edit_gamer/:stationId", {
             controller:"Core",
-            templateUrl:"views/templates/gts_edit_gamer.html",
+            templateUrl:"views/gts_edit_gamer.html",
             authRequired: true
         })
         .when("/admin_user_profile/:displayName/:userId", {
             controller:"Core",
-            templateUrl:"views/templates/admin_user_profile.html",
+            templateUrl:"views/admin_user_profile.html",
             authRequired: true
         })
         .when("/admin_game_info/:gameTitle/:gameId", {
             controller:"Core",
-            templateUrl:"views/templates/admin_game_info.html",
+            templateUrl:"views/admin_game_info.html",
             authRequired: true
         })
         .when("/admin_station_info/:stationNumber/:stationId", {
             controller:"Core",
-            templateUrl:"views/templates/admin_station_info.html",
+            templateUrl:"views/admin_station_info.html",
             authRequired: true
         })
         .when("/admin_system_info/:systemName/:systemId", {
             controller:"Core",
-            templateUrl:"views/templates/admin_system_info.html",
+            templateUrl:"views/admin_system_info.html",
             authRequired: true
         })
         .when("/admin_staff_info/:staffName/:staffId", {
             controller:"Core",
-            templateUrl:"views/templates/admin_staff_info.html",
+            templateUrl:"views/admin_staff_info.html",
             authRequired: true
         })
 
